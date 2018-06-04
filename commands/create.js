17f5e65677e8979578f64400e450106cfbccb27c
NizonRox <nizon.xd@gmail.com>
2018-06-04 11:14:04 +0200

    High key me twice, shame on me.

--- commands/create.js
+++ commands/create.js
@@ -7,7 +7,7 @@ module.exports = {
 		eventdetails = args[1];
 		eventcreator = message.author.username;
 		idofmaker = message.author.id;
-		EmbedMessage();
+		embedmessage();
 		sentMessage = await client.channels.get(host_channel).send({ embed });
 		console.log('\x1b[34m%s\x1b[0m',message.author.username + ' Has created the event: ' + eventname);
 		savecache();
