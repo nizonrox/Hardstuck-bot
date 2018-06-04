d08e7f53d6bb4c8de9676a66e502b9813ca25d7a
NizonRox <nizon.xd@gmail.com>
2018-06-04 11:04:39 +0200

    Cleaning up errors.
    Database should be created as wanted now, commands got the scope they need for their task, colored messaged maintained.
    Red = Error.
    Green = !Error.
    Blue = User interaction.
    White = Chained action.

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const fs = require('fs');  //File System Module
-const Discord = require('discord.js');  //Discord Module
+const fs = require('fs');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token, _comment, admin, host_channel }  = require('./config.json');
 const cache = require('persistent-cache');
 const db = cache();
 
-var ran = '';
 var dbcurrent = '1';
+var sentMessage = '';
 
-//Command Handler Setup
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands');
 for (const file of commandFiles) {
@@ -21,25 +20,26 @@ client.on('ready', () => {
 	client.user.setStatus('online');
 });
 
-
-//Database Hooker $$$
-if (db.keysSync().includes('dbversion') && dbcurrent == db.getSync('dbversion')) {
-	eventname = db.getSync('eventname');
-	eventdetails = db.getSync('eventdetails');
-	eventcreator = db.getSync('eventcreator');
-	eventmembers = db.getSync('eventmembers');
-	eventreserve = db.getSync('eventreserve');
-	idofmaker = db.getSync('idofmaker');
-	messageid = db.getSync('messageid');
-	console.log('\x1b[32m%s\x1b[0m', 'Database was compatible. Loaded!');
-}
-else
-{
+if (db.keysSync().includes('dbversion')) {
+	dbversion = db.getSync('dbversion');
+	console.log('Database found on version: ' + dbversion);
+	if (dbversion == dbcurrent) {
+		eventname = db.getSync('eventname');
+		eventdetails = db.getSync('eventdetails');
+		eventcreator = db.getSync('eventcreator');
+		eventmembers = db.getSync('eventmembers');
+		eventreserve = db.getSync('eventreserve');
+		idofmaker = db.getSync('idofmaker');
+		messageid = db.getSync('messageid');
+		console.log('\x1b[32m%s\x1b[0m', 'Database was compatible. Loaded!');
+	}
+	else
+	{
 	db.putSync('eventname', '0');
 	db.putSync('eventdetails', '');
 	db.putSync('eventcreator', '');
-	db.putSync('eventmembers', '');
-	db.putSync('eventreserve', '');
+	db.putSync('eventmembers', []);
+	db.putSync('eventreserve', []);
 	db.putSync('idofmaker', '');
 	db.putSync('messageid', '');
 	db.putSync('dbversion', dbcurrent);
@@ -49,8 +49,9 @@ else
 	eventmembers = db.getSync('eventmembers');
 	eventreserve = db.getSync('eventreserve');
 	idofmaker = db.getSync('idofmaker');
-	messageid = db.getSync('messageid');
+	message = db.getSync('messageid');
 	console.log('\x1b[31m%s\x1b[0m', 'Database was not compatible or not found. New created and loaded!');
+	};
 };
 
 function savecache() {
@@ -64,7 +65,7 @@ function savecache() {
 	console.log('Database Sync');
 };
 
-function cleanmemory() {
+function cleanmem() {
 	eventname = '0';
 	eventdetails = '';
 	eventcreator = '';
@@ -73,19 +74,45 @@ function cleanmemory() {
 	idofmaker = '';
 }
 
-//Command Handler
-client.on('message', message => {
+function embedmessage() {
+	embed = {
+  "title": eventname,
+  "description": eventdetails,
+  "url": "https://grabify.link/VQHYXV",
+  "color": 0xde21b8,
+  "thumbnail": {
+    "url": "https://i.pinimg.com/originals/93/52/aa/9352aa8fe2011b7da46cf6d0d2e9d328.gif"
+  },
+  "author": {
+    "name": eventcreator,
+    "url": "https://grabify.link/VQHYXV",
+    "icon_url": client.users.get(idofmaker).avatarURL
+  },
+  "fields": [
+    {
+      "name": "Members:",
+      "value": eventmembers + '.',
+    },
+    {
+      "name": "Reserve:",
+      "value": eventreserve + '.',
+    }
+  ]
+};
+};
+
+
+client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length + 1).split(/ +/);
 	const command = args.shift().toLowerCase();
-	ran = args.shift();
 	if (!client.commands.has(command)) return;
 	try {
-		client.commands.get(command).execute(message, args);
+		client.commands.get(command).execute(message, args, embedmessage, client, host_channel, savecache);
 	}
 	catch (error) {
-		console.log('\x1b[34m%s\x1b[0m',message.author.username + ' Executed: ' + ran + ' Causing an error:');
-		console.error('\x1b[31m%s\x1b[0m', error);
+		console.error(error);
+		console.log('\x1b[34m%s\x1b[0m','there was an error trying to execute that command!');
 	}
 });
 	
