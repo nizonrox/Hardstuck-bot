0578293c32fe392183c1b2878d0b5170941171cf
NizonRox <nizon.xd@gmail.com>
2018-06-04 11:05:56 +0200

    Create join.js

---
+++ commands/join.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: 'join',
+	description: 'Joins the event as member or reserve.',
+	async execute(message, args, EmbedMessage, client, host_channel, savecache) {
+		if (!eventname == '0') {
+			if (eventmembers.length <= '9') {
+				eventmembers.push(message.author.username);
+				console.log('\x1b[34m%s\x1b[0m',message.author.username + ' Has been added as a member');
+				}
+			else
+				{
+				eventreserve.push(message.author.username);
+				console.log('\x1b[34m%s\x1b[0m',message.author.username + ' Has been added as a reserve');
+				};
+			EmbedMessage();
+			await sentMessage.edit({ embed });
+			savecache();
+		};
+	},
+};
+
+
